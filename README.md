71c97d615e13867358e3c2f021caebf74753bea8
guhl <guhl@dershampoonierte.com>
2020-10-25 20:42:20 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,14 +1,21 @@
 # <img src="https://raw.githack.com/FortAwesome/Font-Awesome/master/svgs/solid/hand-point-down.svg" card_color="#2DEBA9" width="50" height="50" style="vertical-align:bottom"/> Wake Button Rpi
-Gpio connected button instead of wake word
+Usd a GPIO connected button instead of wake word
 
 ## About
-Press button that is connected to a gpio input board to trigger wakeup
+Press button that is connected to a GPIO port to trigger wakeup so that the device starts listening. 
+If the button is pressed for a longer time he stops whatever he is dooing.
+This skill is heavily based on the Google AIY voicekit skill by Andlo.
 
-## Examples
-* "Gpio connected button instead of wake word"
+## Important
+The skill needs to be able to access device /dev/gpiomem.
+Make sure that this device is read/writable for the user or group running mycroft.
+You may have to adapt the udev rules and add the mycroft user to the gpio group.
+By default the skill expects the button to be a pull up button connected to GPIO 17 (button connected to +3.3V and GPIO 17).
+The GPIO number can be changed in the settings.
 
 ## Credits
-Guhl
+Guhl (@guhl)
+Andreas Lorensen (@andlo)
 
 ## Category
 **IoT**
--- __init__.py
+++ __init__.py
@@ -1,14 +1,51 @@
 from mycroft import MycroftSkill, intent_file_handler
+from mycroft.messagebus.message import Message
 
+import time
+import RPi.GPIO as GPIO
+
+BUTTON = 17
 
 class WakeButtonRpi(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('rpi.button.wake.intent')
-    def handle_rpi_button_wake(self, message):
-        self.speak_dialog('rpi.button.wake')
+    def initialize(self):
+        try:
+            BUTTON = self.settings.get('gpio_port', 17)
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+            GPIO.add_event_detect(BUTTON, GPIO.RISING, bouncetime = 500)
+        except GPIO.error:
+            self.log.warning("Can't initialize GPIO - skill will not load")
+            self.speak_dialog("error.initialise")
+        finally:
+            self.schedule_repeating_event(self.handle_button,
+                                          None, 0.1, 'WakeButtonRpi')
+            self.add_event('recognizer_loop:record_begin',
+                           self.handle_listener_started)
+            self.add_event('recognizer_loop:record_end',
+                           self.handle_listener_ended)
 
+    def handle_button(self, message):
+        longpress_threshold = 2
+        if GPIO.event_detected(BUTTON):
+            self.log.info("GPIO.event_detected")
+            pressed_time = time.time()
+            while not GPIO.input(BUTTON):
+                time.sleep(0.2)
+            pressed_time = time.time() - pressed_time
+            if pressed_time < longpress_threshold:
+                self.bus.emit(Message("mycroft.mic.listen"))
+            else:
+                self.bus.emit(Message("mycroft.stop"))
+
+    def handle_listener_started(self, message):
+        # code to excecute when active listening begins...
+        self.log.info("WakeButtonRpi listener started")
+
+    def handle_listener_ended(self, message):
+        self.log.info("WakeButtonRpi listener ended")
 
 def create_skill():
     return WakeButtonRpi()
